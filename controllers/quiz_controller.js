f604527e9d98e09922d1cf9a77ab373c7514bc20
miguel-macias-cursoNode <m8r-nrrn4g@suremail.info>
2015-07-13 19:42:07 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,11 @@
+﻿exports.question = function (req, res) {
+    res.render('quizes/question',
+               { pregunta: "¿Capital de Italia?" });
+};
+exports.answer= function (req, res) {
+    res.render('quizes/answer',
+               { respuesta: (/^\s*roma\s*$/i.test(req.query.respuesta))? 
+                   "Correcto":
+                   "Incorrecto"
+    });
+};
