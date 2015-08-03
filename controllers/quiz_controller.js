46bc8cb6026a8f5998b7576d4c203447935eb81e
miguel-macias-cursoNode <m8r-nrrn4g@suremail.info>
2015-08-03 21:03:57 +0200

    mod7p3

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,32 +1,44 @@
 ﻿var models = require('../models/models');
 
-exports.show = function (req, res) {
-    models.Quiz.find(req.params.quizId).then(
+// AutoLoad: factoriza el código si la ruta incluye :quizId
+exports.load = function (req, res, next, quizId) {
+    models.Quiz.find(quizId)
+      .then(
         function (quiz) {
-            res.render(
-                'quizes/show',
-               { quiz: quiz });
+            if (quiz) {
+                req.quiz = quiz;
+                next();
+            }
+            else {
+                next(new Error("No existe la pregunta " + quizId));
+            }
+        })
+      .catch(
+        function (error) {
+            next(error);
         }
-    );
+      );
+};
+
+exports.show = function (req, res) {
+    res.render(
+        'quizes/show',
+        { quiz: req.quiz });
 };
 exports.answer = function (req, res) {
-    models.Quiz.find(req.params.quizId).then(
-        function (quiz) {
-            res.render(
-                'quizes/answer',
-               {
-                   quiz: quiz,
-                    respuesta: (req.query.respuesta.trim().toLowerCase() == quiz.respuesta.toLowerCase())? 
+    res.render(
+        'quizes/answer',
+        {
+            quiz: req.quiz,
+            respuesta: (req.query.respuesta.trim().toLowerCase() == req.quiz.respuesta.toLowerCase())? 
                    "Correcto":
                    "Incorrecto"
-                });
-        }
-    );
+        });
 };
 exports.index = function (req, res) {
     models.Quiz.findAll().then(
         function (quizes) {
             res.render('quizes/index.ejs', { quizes: quizes });
         }
-    );
+    ).catch(function (error) { next(error); });
 };
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* Autoload de comandos */
+router.param('quizId', quizController.load);
+
 /* Quizes */
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
