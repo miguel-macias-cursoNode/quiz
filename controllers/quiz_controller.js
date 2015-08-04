18836a1d233e000fa8912f037971e93a42f365ee
miguel-macias-cursoNode <m8r-nrrn4g@suremail.info>
2015-08-04 10:53:29 +0200

    m8p4

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -108,3 +108,17 @@ exports.update = function (req, res) {
         }
     );
 };
+exports.destroy = function (req, res) {
+    req.quiz.destroy()
+        .then(
+            function () {
+                res.redirect('/quizes');
+            }
+        )
+        .catch(
+            function (error) {
+                next(error);
+            }
+        );
+};
+
