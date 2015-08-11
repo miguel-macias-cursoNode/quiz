e8f5a8810183773bcb781a51fb4e8a435f5309e0
miguel-macias-cursoNode <m8r-nrrn4g@suremail.info>
2015-08-11 17:54:51 +0200

    m9tarea2a

---
+++ controllers/statistics_controller.js
@@ -0,0 +1,12 @@
+﻿var models = require('../models/models');
+
+exports.show = function (req, res) {
+    var datos = {
+        questions: 0,
+        comments: 0,
+        com_question: 0,
+        quest_nocom: 0,
+        quest_comm: 0
+    };
+    res.render('statistics/show', { statistics: datos });
+};
